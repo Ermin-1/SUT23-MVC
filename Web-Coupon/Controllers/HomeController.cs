dab4c81cc065e13e73fc3e2221369320b30fcb80
Ermin Husic <Ermin.Husic@live.se>
2024-09-03 14:39:01 +0200

    update

--- Web-Coupon/Controllers/HomeController.cs
+++ Web-Coupon/Controllers/HomeController.cs
@@ -23,6 +23,8 @@ namespace Web_Coupon.Controllers
             return View();
         }
 
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
